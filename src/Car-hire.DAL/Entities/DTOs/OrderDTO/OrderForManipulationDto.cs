2c5d100b9821a0cb4d52ed8f73611902d3154f0e
siviavio4ka <siviavio4ka@gmail.com>
2024-04-16 17:56:13 +0300

    refactor: move properties to dtos for order

--- src/Car-hire.DAL/Entities/DTOs/OrderDTO/OrderForManipulationDto.cs
+++ src/Car-hire.DAL/Entities/DTOs/OrderDTO/OrderForManipulationDto.cs
@@ -4,18 +4,9 @@ namespace Car_hire.DAL.Entities.DTOs.OrderDTO;
 
 public abstract record OrderForManipulationDto
 {
-    [Required(ErrorMessage = "Order number is required field")]
-    [RegularExpression(@"^\d{5}$", ErrorMessage = "Order number must be a 5-digit number")]
-    public int OrderNumber { get; init; }
-    [Required(ErrorMessage = "Car id is required field")]
-    public int CarId { get; init; }
     [Required(ErrorMessage = "Order date is required field")]
     public DateTime OrderDate { get; init; }
-    [Required(ErrorMessage = "User id is required field")]
-    public int UserId { get; init; }
     [Required(ErrorMessage = "Hire hours is required field")]
     [Range(1, int.MaxValue, ErrorMessage = "Hire hours must be a positive number")]
     public int HireHours { get; init; }
-    public DateTime Deadline { get; init; }
-    public double? Amount { get; init; }
 }
