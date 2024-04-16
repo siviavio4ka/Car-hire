5ed1ebd9a4b37dfdd5d8b5d374bf7fc452cc9742
siviavio4ka <siviavio4ka@gmail.com>
2024-04-16 17:36:54 +0300

    feat: update user generator

--- src/Car-hire.DAL/Infrastructure/EntityGenerators/UserGenerator.cs
+++ src/Car-hire.DAL/Infrastructure/EntityGenerators/UserGenerator.cs
@@ -1,4 +1,5 @@
 using Car_hire.DAL.Entities.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Car_hire.DAL.Infrastructure.EntityGenerators;
 
@@ -48,24 +49,43 @@ public static class UserGenerator
     };
 
     public static IEnumerable<User> GenerateUser(int count, int minValue, int maxValue, int numberOfStreet) =>
-        Enumerable.Range(1, count).Select(i => new User
+        Enumerable.Range(1, count).Select(i =>
         {
-            UserId = i,
-            Name = GenerateName(),
-            PassportDetails = GeneratePassportDetails(minValue, maxValue),
-            Address = GenerateAddress(numberOfStreet),
-            PhoneNumber = GeneratedPhoneNumber()
+            var name = GenerateName();
+            var userName = GenerateUserName(name);
+            var email = GenerateEmail(name);
+            return new User
+            {
+                Id = i,
+                Name = name,
+                UserName = userName,
+                NormalizedUserName = userName.ToUpper(),
+                Email = email,
+                NormalizedEmail = email.ToUpper(),
+                PasswordHash = new PasswordHasher<User>().HashPassword(null, "1234567890"),
+                PassportDetails = GeneratePassportDetails(minValue, maxValue),
+                Address = GenerateAddress(numberOfStreet),
+                PhoneNumber = GeneratedPhoneNumber(),
+                SecurityStamp = Guid.NewGuid().ToString(),
+                LockoutEnabled = true
+            };
         });
 
     private static string GenerateName() =>
         $"{FirstNames[Random.Next(FirstNames.Count)]} {LastNames[Random.Next(LastNames.Count)]}";
+
+    private static string GenerateUserName(string name) =>
+        name.Replace(" ", "").ToLower();
+
+    private static string GenerateEmail(string name) =>
+        $"{name.Replace(" ", "").ToLower()}{Random.Next(10, 99)}@gmail.com";
 
     private static string GenerateAddress(int numberOfStreet) =>
         $"{Random.Next(1, numberOfStreet)} {Streets[Random.Next(Streets.Count)]}, {Cities[Random.Next(Cities.Count)]}";
 
     private static string GeneratePassportDetails(int minValue, int maxValue) =>
         (char)('A' + Random.Next(26)) + "" + (char)('A' + Random.Next(26)) + Random.Next(minValue, maxValue);
-    
+
     private static string GeneratedPhoneNumber() =>
         $"+375 {TelephoneNumberCodes[Random.Next(TelephoneNumberCodes.Count)]} {Random.Next(1000000, 9999999)}";
 }
