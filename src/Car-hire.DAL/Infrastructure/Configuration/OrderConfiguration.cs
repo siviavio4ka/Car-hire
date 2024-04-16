4df44b63076a749f1bff09f9c5aa9f166f03cc3f
siviavio4ka <siviavio4ka@gmail.com>
2024-04-16 17:40:28 +0300

    refactor: remove commented code

--- src/Car-hire.DAL/Infrastructure/Configuration/OrderConfiguration.cs
+++ src/Car-hire.DAL/Infrastructure/Configuration/OrderConfiguration.cs
@@ -9,11 +9,6 @@ public class OrderConfiguration : IEntityTypeConfiguration<Order>
 {
     public void Configure(EntityTypeBuilder<Order> builder)
     {
-        // builder.HasOne(o => o.User)
-        //        .WithMany(c => c.Orders)
-        //        .HasForeignKey(o => o.CustomerId)
-        //        .HasPrincipalKey(c => c.CustomerId);
-        
         var orders = OrderGenerator.GenerateOrder(50, 30, new DateTime(2000, 1, 1, 0, 0, 0), 30, 168);
         builder.HasData(orders);
     }
